bb0d17968ff59acb92d5e586487a08bc2af146f8
Damali Baker <damalib11@gmail.com>
2026-06-01 15:59:08 -0400

    Add user entity and repository

---
+++ src/main/java/io/github/damalibaker/commerceapi/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package io.github.damalibaker.commerceapi.entity;
+
+import io.github.damalibaker.commerceapi.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
